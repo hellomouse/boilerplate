031ae61b7db22643e807303d02c2b871ba9c7e78
iczero <iczero4@gmail.com>
2020-05-02 23:43:29 -0700

    fix configuration

--- server/config.ts
+++ server/config.ts
@@ -5,17 +5,19 @@ const env = process.env;
 /**
  * Define a required configuration parameter
  * @param name Configuration value name
- * @param mapFn Value map function
  * @return
  */
-function required<T>(name: string, mapFn: (a: string) => T): T {
+function required(name: string): string {
   let value = env[name];
   if (typeof value === 'undefined') {
     throw new Error(`required value ${name} not provided`);
   }
-  return mapFn(value);
+  return value;
 }
 
-export const BASE_URL = required('BASE_URL', url => new URL(url));
-export const API_BASE_URL = required('API_BASE_URL', url => new URL(url));
-export const PORT = required('PORT', port => +port);
+export const BASE_URL = new URL(required('BASE_URL'));
+export const API_BASE_URL = new URL(required('API_BASE_URL'));
+export const PORT = +required('PORT');
+export const TLS = env.TLS === 'true';
+export const TLS_CERT = TLS ? required('TLS_CERT') : null;
+export const TLS_KEY = TLS ? required('TLS_KEY') : null;
--- server/server.ts
+++ server/server.ts
@@ -5,20 +5,19 @@ import http from 'http';
 import https from 'https';
 import app from './app';
 import log from './logger';
-import { PORT } from './config';
-const env = process.env;
+import * as config from './config';
 
 /** Main function */
 async function main() {
   /** @type {http.Server | https.Server} */
   let server;
-  if (env.TLS === 'true') {
-    if (!env.TLS_CERT || !env.TLS_KEY) throw new Error('no tls key or cert specified');
+  if (config.TLS) {
+    if (!config.TLS_CERT || !config.TLS_KEY) throw new Error('no tls key or cert specified');
     server = https.createServer({
-      cert: await fsP.readFile(env.TLS_CERT),
-      key: await fsP.readFile(env.TLS_KEY)
+      cert: await fsP.readFile(config.TLS_CERT),
+      key: await fsP.readFile(config.TLS_KEY)
     }, app);
   } else server = http.createServer(app);
-  server.listen(PORT).on('listening', () => log.info('Listening'));
+  server.listen(config.PORT).on('listening', () => log.info('Listening'));
 }
 main();
